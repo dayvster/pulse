4ae7218ad7483f2530423b47bd3fc475b4d7c59c
Dayvi Schuster <dayvi@arctek.dev>
2023-09-14 08:52:47 +0200

    removed psmode

--- src/main.rs
+++ src/main.rs
@@ -28,19 +28,6 @@ fn main() {
         let pid: u32 = args.pid.unwrap().parse::<u32>().unwrap();
         let interval = args.interval.unwrap_or(1.0);
         loop_output(pid, &utils.get_name(&pid), interval, &term, &utils);
-    } else if args.psmode.unwrap() == true {
-        // get top 6 processes by cpu usage just like ps
-        let mut processes: Vec<(u32, String, f64)> = Vec::new();
-        for p in utils.get_collector().processes.iter() {
-            let pid = p.1.pid();
-            let name = p.1.name().unwrap();
-            let cpu = utils.get_cpu(&pid).unwrap();
-            processes.push((pid, name, cpu));
-        }
-        processes.sort_by(|a, b| b.2.partial_cmp(&a.2).unwrap());
-        for p in processes.iter().take(6) {
-            println!("{} {} {}", p.0, p.1, p.2);
-        }
     } else {
         println!("You need to specify either a pid or a name.");
         println!("Seek help with --help or -h");
